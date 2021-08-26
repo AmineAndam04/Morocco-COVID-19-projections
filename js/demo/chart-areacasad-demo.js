67ada92beeae4374933bf9ff5519a038c11d3337
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2021-08-26 16:07:02 +0100

    Update chart-areacasad-demo.js

--- js/demo/chart-areacasad-demo.js
+++ js/demo/chart-areacasad-demo.js
@@ -32,11 +32,11 @@ var ctx = document.getElementById("myAreaChart2");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["22/08/2021",
-"23/08/2021",
-"24/08/2021",
-"25/08/2021",
-"26/08/2021"],
+    labels: ["26/08/2021",
+"27/08/2021",
+"28/08/2021",
+"29/08/2021",
+"90/08/2021"],
     datasets: [{
       label: "Deaths",
       lineTension: 0.3,
@@ -51,11 +51,11 @@ var myLineChart = new Chart(ctx, {
       pointHitRadius: 10,
       pointBorderWidth: 2,
       data: [
-16,
+15,
+19,
 17,
 15,
-13,
-15
+19
 ],
     }],
   },
