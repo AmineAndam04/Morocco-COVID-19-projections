a908197a4c5c6099b7f2b5ee762a2c1d7aef9354
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2021-08-26 16:28:57 +0100

    Update chart-arealayond-demo.js

--- js/demo/chart-arealayond-demo.js
+++ js/demo/chart-arealayond-demo.js
@@ -33,11 +33,11 @@ var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: [
-"22/08/2021",
-"23/08/2021",
-"24/08/2021",
-"25/08/2021",
-"26/08/2021"],
+"26/08/2021",
+"27/08/2021",
+"28/08/2021",
+"29/08/2021",
+"30/08/2021"],
     datasets: [{
       label: "Deaths",
       lineTension: 0.3,
@@ -52,11 +52,11 @@ var myLineChart = new Chart(ctx, {
       pointHitRadius: 10,
       pointBorderWidth: 2,
       data: [
-6,
 4,
-5,
-6,
-6
+2,
+0,
+0,
+1
 ],
     }],
   },
