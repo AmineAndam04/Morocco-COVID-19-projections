6aafa770bbd61909aca916fdf7cb98c364e09de1
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2021-08-26 16:18:07 +0100

    Update chart-areatanja-demo.js

--- js/demo/chart-areatanja-demo.js
+++ js/demo/chart-areatanja-demo.js
@@ -33,11 +33,11 @@ var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: [
-"22/08/2021",
-"23/08/2021",
-"24/08/2021",
-"25/08/2021",
-"26/08/2021"],
+"26/08/2021",
+"27/08/2021",
+"28/08/2021",
+"29/08/2021",
+"30/08/2021"],
     datasets: [{
       label: "New Cases",
       lineTension: 0.3,
@@ -52,11 +52,11 @@ var myLineChart = new Chart(ctx, {
       pointHitRadius: 10,
       pointBorderWidth: 2,
       data: [
-904,
-988,
-1105,
-1168,
-1284
+1007,
+978,
+764,
+409,
+336
 ],
     }],
   },
