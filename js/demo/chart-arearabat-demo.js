96fee0a5ed39812488c6d9acfc6f40393b902601
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2021-08-26 16:16:08 +0100

    Update chart-arearabat-demo.js

--- js/demo/chart-arearabat-demo.js
+++ js/demo/chart-arearabat-demo.js
@@ -33,11 +33,11 @@ var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: [
-"22/08/2021",
-"23/08/2021",
-"24/08/2021",
-"25/08/2021",
-"26/08/2021"],
+"26/08/2021",
+"27/08/2021",
+"28/08/2021",
+"29/08/2021",
+"30/08/2021"],
     datasets: [{
       label: "New Cases",
       lineTension: 0.3,
@@ -52,11 +52,11 @@ var myLineChart = new Chart(ctx, {
       pointHitRadius: 10,
       pointBorderWidth: 2,
       data: [
-1214,
-1160,
-1139,
-1151,
-1098
+1263,
+1204,
+1192,
+1137,
+1131
 ],
     }],
   },
