dfa2ec27f6a4d60df97d51ef4a084d046ace2080
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2021-08-31 15:51:43 +0100

    Update chart-areacasa-demo.js

--- js/demo/chart-areacasa-demo.js
+++ js/demo/chart-areacasa-demo.js
@@ -32,11 +32,11 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["26/08/2021",
-"27/08/2021",
-"28/08/2021",
-"29/08/2021",
-"30/08/2021"],
+    labels: ["31/08/2021",
+"01/09/2021",
+"02/09/2021",
+"03/09/2021",
+"04/09/2021"],
     datasets: [{
       label: "New Cases",
       lineTension: 0.3,
@@ -50,12 +50,11 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [
-2461,
-2034,
-1141,
-692,
-1370,
+      data: [2167,
+2286,
+2283,
+2191,
+1755
 ],
     }],
   },
